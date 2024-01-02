002a12f4e156cf099d9b038aaceeb8ceea6c3955
aybika <adamovaaybika@mail.ru>
2024-01-02 17:02:36 +0300

    initial commit

---
+++ src/theme/ThemeContext.ts
@@ -0,0 +1,14 @@
+import {createContext} from 'react'
+export enum Theme {
+    LIGHT= "light",
+    DARK = 'dark',
+  
+  }
+  export interface ThemeContexttstsProps {
+    theme?: Theme;
+    setTheme?: (theme: Theme) => void;
+  }
+
+  export const ThemeContext = createContext<ThemeContexttstsProps>({})
+
+  export const LOCAL_STORAGE_THEME_KEY = 'theme';
