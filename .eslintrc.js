fede4ffa458573ec0ccf0f07f0b6a78868601513
aybika <adamovaaybika@mail.ru>
2024-01-07 13:25:25 +0300

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,10 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
-        'plugin:react/recommended', 
+        'plugin:react/recommended',
         'plugin:i18next/recommended',
         'airbnb',
     ],
